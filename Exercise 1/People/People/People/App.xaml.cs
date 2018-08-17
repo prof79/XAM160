187f603b8d34ea67b722524f5a800f89e15a7310
prof79 <markus@markusegger.at>
2018-08-17 22:57:26 +0200

    Projects created and configured.

---
+++ Exercise 1/People/People/People/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿// App.xaml.cs
+
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace People
+{
+    using System;
+    using Xamarin.Forms;
+
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Exercise 1/People/People/People/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿// MainPage.xaml.cs
+
+namespace People
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xamarin.Forms;
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

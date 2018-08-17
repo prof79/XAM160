de5c9fa439190debd059af291af34d4e8e91d103
prof79 <markus@markusegger.at>
2018-08-17 23:37:46 +0200

    Added file access helper platform services plus debug display.

---
+++ Exercise 1/People/People/People.Android/Utility/FileAccessHelper_Android.cs
@@ -0,0 +1,44 @@
+﻿//----------------------------------------------------------------------------
+// <copyright file="FileAccessHelper_Android.cs"
+//      company="Markus M. Egger">
+//      Copyright (C) 2018 Markus M. Egger. All rights reserved.
+// </copyright>
+// <author>Markus M. Egger</author>
+// <description>
+//      Implementation of platform-specific file operations for Android.
+// </description>
+// <version>v0.9.0 2018-08-17T23:25:00+02</version>
+//
+// Based on Xamarin University guidance and XAM160.
+//
+//----------------------------------------------------------------------------
+
+[assembly: Xamarin.Forms.Dependency(typeof(People.Droid.Utility.FileAccessHelper_Android))]
+
+namespace People.Droid.Utility
+{
+    using System;
+    using System.IO;
+    using People.Utility;
+
+    /// <summary>
+    /// Implementation of platform-specific file operations for Android.
+    /// </summary>
+    public class FileAccessHelper_Android : IFileAccessHelper
+    {
+        #region Interface IFileAccessHelper
+
+        /// <inheritdoc />
+        public string GetLocalFilePath(string fileName)
+        {
+            var personalFolder =
+                Environment
+                    .GetFolderPath(
+                        Environment.SpecialFolder.Personal);
+
+            return Path.Combine(personalFolder, fileName);
+        }
+
+        #endregion
+    }
+}
--- Exercise 1/People/People/People/MainPage.xaml.cs
+++ Exercise 1/People/People/People/MainPage.xaml.cs
@@ -3,17 +3,36 @@
 namespace People
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using Xamarin.Forms;
+    using Utility;
 
     public partial class MainPage : ContentPage
     {
+        #region Fields
+
+        private readonly IFileAccessHelper _fileAccessHelper;
+
+        #endregion
+
         public MainPage()
         {
+            _fileAccessHelper =
+                DependencyService.Get<IFileAccessHelper>();
+
             InitializeComponent();
+
+            Text = _fileAccessHelper.GetLocalFilePath(String.Empty);
         }
+
+        #region Properties
+
+        public string Text
+        {
+            get => textLabel.Text;
+
+            set => textLabel.Text = value;
+        }
+
+        #endregion
     }
 }
